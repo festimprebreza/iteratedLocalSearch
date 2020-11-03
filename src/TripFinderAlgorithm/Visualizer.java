f25fbd52a5f213154241a5eddefb255c1b1601a4
Festim <festimprebreza@outlook.com>
2020-11-03 19:20:42 +0100

    added map drawing

--- src/TripFinderAlgorithm/Visualizer.java
+++ src/TripFinderAlgorithm/Visualizer.java
@@ -10,6 +10,7 @@ public class Visualizer extends JPanel {
 	private String ILSStatus = "Sample message";
 	private boolean waiting = false;
 	private int currentBestScore = 0;
+	private boolean drawingMap = true;
 	private Color[] colors = {
 		Color.decode("#B2AA8E"),
 		Color.decode("#0C1B33"),
@@ -22,12 +23,29 @@ public class Visualizer extends JPanel {
 		Color.decode("#7FBEEB"),
 		Color.decode("#FF4365")};
 
+	private Color[] mapColor = {
+		Color.RED,
+		Color.GREEN,
+		Color.BLUE,
+		Color.YELLOW
+	};
+
 	public Visualizer(Solution currentSolution) {
 		this.currentSolution = currentSolution;
 	}
 
 	public void paint(Graphics g) {
 		super.paintComponent(g);
+		Graphics2D g2 = drawDefault(g);
+		if(drawingMap) {
+			drawMap(g2);
+		}
+		else {
+			drawTimeline(g2);
+		}		
+	}
+
+	public Graphics2D drawDefault(Graphics g) {
 		Graphics2D g2 = (Graphics2D)g;
 		this.setBackground(Color.WHITE);
 		if(currentSolution.getScore() > currentBestScore) {
@@ -38,8 +56,72 @@ public class Visualizer extends JPanel {
 		g2.setColor(Color.BLACK);
 		g2.setFont(new Font("Times New Roman", Font.PLAIN, 32));
 		g2.drawString(String.format("%.2f", (currentSolution.getScore() / 100.0f)), 50, 50);
-		g2.drawString(message, 50, 850);
-		g2.drawString(ILSStatus, 650, 850);
+		g2.drawString(message, 250, 950);
+		g2.drawString(ILSStatus, 850, 950);
+
+		return g2;
+	}
+
+	public void drawMap(Graphics2D g2) {
+		// get extreme coordinates
+		long lowestX = currentSolution.getProblemInput().getStartingPOI().getXCoordinate();
+		long lowestY = currentSolution.getProblemInput().getStartingPOI().getYCoordinate();
+		long highestY = currentSolution.getProblemInput().getStartingPOI().getYCoordinate();
+		for(POI currentPOI: currentSolution.getProblemInput().getVisitablePOIs()) {
+			if(currentPOI.getXCoordinate() < lowestX) {
+				lowestX = currentPOI.getXCoordinate();
+			}
+			if(currentPOI.getYCoordinate() < lowestY) {
+				lowestY = currentPOI.getYCoordinate();
+			}
+			if(currentPOI.getYCoordinate() > highestY) {
+				highestY = currentPOI.getYCoordinate();
+			}
+		}
+		long toShiftX = 0;
+		long toShiftY = 0;
+		if(lowestX < 0) {
+			toShiftX = -lowestX;
+		}
+		if(lowestY < 0) {
+			toShiftY = -lowestY;
+		}
+		// draw points
+		int circleRadius = 10;
+		int startingX = 50;
+		int startingY = 930;
+		g2.setColor(Color.GRAY);
+		for(POI currentPOI: currentSolution.getProblemInput().getVisitablePOIs()) {
+			int xCoordinate = startingX + (int)((currentPOI.getXCoordinate() + toShiftX) / 100);
+			int yCoordinate = startingY - (int)((currentPOI.getYCoordinate() + toShiftY) / 100);
+			g2.fillOval(xCoordinate - circleRadius, yCoordinate - circleRadius, circleRadius * 2, circleRadius * 2);
+		}
+		int xCoordinate = startingX + (int)((currentSolution.getProblemInput().getStartingPOI().getXCoordinate() + toShiftX) / 100);
+		int yCoordinate = startingY - (int)((currentSolution.getProblemInput().getStartingPOI().getYCoordinate() + toShiftY) / 100);
+		g2.setColor(Color.BLACK);
+		g2.fillOval(xCoordinate - circleRadius, yCoordinate - circleRadius, circleRadius * 2, circleRadius * 2);
+		// draw lines
+		for(int tour = 0; tour < currentSolution.getProblemInput().getTourCount(); tour++) {
+			POIInterval currentPOIInterval = currentSolution.getNthPOIIntervalInTourX(0, tour).getPreviousPOIInterval();
+			while(currentPOIInterval.getPOI() != currentSolution.getProblemInput().getEndingPOI()) {
+				g2.setStroke(new BasicStroke(2.5f));
+				g2.setColor(mapColor[tour]);
+				int startingXCoord = startingX + (int)((currentPOIInterval.getPOI().getXCoordinate() + toShiftX) / 100);
+				int startingYCoord = startingY - (int)((currentPOIInterval.getPOI().getYCoordinate() + toShiftY) / 100);
+				int endingXCoord = startingX + (int)((currentPOIInterval.getNextPOIInterval().getPOI().getXCoordinate() + toShiftX) / 100);
+				int endingYCoord = startingY - (int)((currentPOIInterval.getNextPOIInterval().getPOI().getYCoordinate() + toShiftY) / 100);
+
+				g2.drawLine(startingXCoord, startingYCoord, endingXCoord, endingYCoord);
+				if(currentPOIInterval.getPOI().getID() != 0) {
+					g2.setColor(colors[currentPOIInterval.getAssignedType()]);
+					g2.fillOval(startingXCoord - circleRadius, startingYCoord - circleRadius, circleRadius * 2, circleRadius * 2);
+				}
+				currentPOIInterval = currentPOIInterval.getNextPOIInterval();
+			}
+		}
+	}
+
+	public void drawTimeline(Graphics2D g2) {
 		// draw patterns
 		int patternSize = 20;
 		for(int tour = 0; tour < currentSolution.getProblemInput().getTourCount(); tour++) {
